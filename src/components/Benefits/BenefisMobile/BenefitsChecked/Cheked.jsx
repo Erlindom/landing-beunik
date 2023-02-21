12d06becf878786f2a1b51e2932bfea0fccd33d1
Erlin Dominguez <erlind299@gmail.com>
2023-02-21 16:02:29 -0500

    Adding iconCheck to BenefitsMobile section

--- src/components/Benefits/BenefisMobile/BenefitsChecked/Cheked.jsx
+++ src/components/Benefits/BenefisMobile/BenefitsChecked/Cheked.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import "../BenefitsChecked/Cheked.css";
+import iconCheck from "../../../Images/icon_checklist.svg";
 
 function Cheked(props) {
   return (
     <>
       <div className="checked_content">
-        <img
-          className="icon_check"
-          src={require("../../../Images/checked.png")}
-          alt=""
-        />
+        <img className="icon_check" src={iconCheck} alt="" />
         <p className="textChecked">{props.benefits}</p>
       </div>
     </>
