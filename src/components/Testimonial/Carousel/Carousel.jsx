763822e877e6d830b31e61097fd8ccc5b1516a40
Erlin Dominguez <erlind299@gmail.com>
2023-02-22 23:55:22 -0500

    Adding carousel for Desktop in Testimonial Section

---
+++ src/components/Testimonial/Carousel/Carousel.jsx
@@ -0,0 +1,107 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import iconStarYellow from "../../Images/icon_StartYellow.svg";
+import "../Carousel/Carousel.css";
+
+function Carousel({ testimonie }) {
+  const [initialPosition, setInitialPosition] = useState(0);
+  const [finalPosition, setFinalPosition] = useState(3);
+  const [testimonieToShow, setTestimonieToShow] = useState([]);
+
+  useEffect(() => {
+    getTestimonieToShowInitialState();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getTestimonieToShowInitialState = () => {
+    const setNewState = testimonie.slice(0, 3);
+    setTestimonieToShow(setNewState);
+  };
+
+  const handleTestimonieToShowChange = (action) => {
+    if (action === "next") {
+      if (finalPosition !== testimonie.length) {
+        const newInitialPosition = initialPosition + 1;
+        const newFinalPosition = finalPosition + 1;
+        setInitialPosition(newInitialPosition);
+        setFinalPosition(newFinalPosition);
+        const newArray = testimonie.slice(newInitialPosition, newFinalPosition);
+        setTestimonieToShow(newArray);
+      } else {
+        setInitialPosition(0);
+        setFinalPosition(3);
+        getTestimonieToShowInitialState();
+      }
+    }
+    if (action === "prev") {
+      if (initialPosition !== 0) {
+        const newInitialPosition = initialPosition - 1;
+        const newFinalPosition = finalPosition - 1;
+        setInitialPosition(newInitialPosition);
+        setFinalPosition(newFinalPosition);
+        const newArray = testimonie.slice(newInitialPosition, newFinalPosition);
+        setTestimonieToShow(newArray);
+      } else {
+        setInitialPosition(0);
+        setFinalPosition(3);
+        getTestimonieToShowInitialState();
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="carousel-container">
+        <div className="testimonies-content">
+          <button
+            className="arrow-btn"
+            onClick={() => handleTestimonieToShowChange("prev")}
+          >
+            <img
+              className="arrow-img"
+              src={require("../../Images/LEFT_ARROW.png")}
+              alt="Flecha para mover hacía la izquierda"
+            />
+          </button>
+          {testimonieToShow.map((testimonieShow) => {
+            return (
+              <div className="testimonie" key={testimonieShow.id}>
+                <div className="avatar-content">
+                  <img
+                    className="icon-avatar"
+                    width={50}
+                    src={require(`../../Images/${testimonieShow.img}.jpeg`)}
+                    alt=""
+                  />
+                  <p className="avatar-name">{testimonieShow.name}</p>
+                  <div className="starYellow-content">
+                    <img className="starYellow" src={iconStarYellow} alt="" />
+                    <img className="starYellow" src={iconStarYellow} alt="" />
+                    <img className="starYellow" src={iconStarYellow} alt="" />
+                    <img className="starYellow" src={iconStarYellow} alt="" />
+                    <img className="starYellow" src={iconStarYellow} alt="" />
+                  </div>
+                  <div>
+                    <p className="text-testimonie">{testimonieShow.desc}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          <button
+            className="arrow-btn"
+            onClick={() => handleTestimonieToShowChange("next")}
+          >
+            <img
+              className="arrow-img"
+              src={require("../../Images/RIGHT_ARROW.png")}
+              alt="Flecha para mover hacía la derecha"
+            />
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Carousel;
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,13 +1,68 @@
 import React from "react";
 import Testimonies from "./Testimonies/Testimonies";
 import "../Testimonial/Testimonial.css";
+import Carousel from "./Carousel/Carousel";
+
+const testimonies = [
+  {
+    id: 1,
+    name: "Big Boss",
+    img: "Bigboss",
+    desc: "Mis clientes lo agradecen, porque a veces no tengo tiempo para responderles al WhatsApp y ya pueden apartar su turno sin necesidad de esperar mi respuesta.",
+  },
+  {
+    id: 2,
+    name: "Círculo Rojo",
+    img: "CirculoRojo",
+    desc: "Hace que mi negocio se vea más profesional.",
+  },
+  {
+    id: 3,
+    name: "Violetta Spa",
+    img: "ViolettaSpa",
+    desc: "Me gusta mucho como puedo administrar mi agenda, y también la plataforma se ha preocupado por añadir funciones que son relevantes para nosotros como dueños de negocio.",
+  },
+  {
+    id: 4,
+    name: "Juan Villa",
+    img: "JuanVilla",
+    desc: "Estoy sorprendido del orden que ha generado en mi negocio, realmente agradezco mucho a la empresa por esta herramienta que nos dieron al gremio.",
+  },
+  {
+    id: 5,
+    name: "YeeSeer",
+    img: "YeeSeer",
+    desc: "Me ahorra tiempo, desde el día jueves ya tengo el fin de semana totalmente agendado.",
+  },
+  {
+    id: 6,
+    name: "Y&M STUDIO",
+    img: "CirculoRojo",
+    desc: "Definitivamente, la tecnología es el camino, tenemos una fundación en Bello y sabemos lo importante que es educar a las personas en esta nueva era digital.",
+  },
+  {
+    id: 7,
+    name: "Hipton",
+    img: "CirculoRojo",
+    desc: "Tenía temor de empezar porque no soy muy bueno con la tecnología, pero mi clientela fue muy receptiva y el manejo de la App muy sencillo.",
+  },
+  {
+    id: 8,
+    name: "Erika Nails Spa",
+    img: "ErikaNailSpa",
+    desc: "Una herramienta muy completa que me brinda facilidad para manejar la liquidación de mi equipo de trabajo y el control de mis citas.",
+  },
+];
 
 function Testimonial() {
   return (
     <section className="testimonial_content" id="testimonial">
       <div className="testimonial">
         <h5 className="title-testimonial">Conoce la experiencia del gremio</h5>
-        <div className="testimonies-content">
+        <div className="testimonial_carousel">
+          <Carousel testimonie={testimonies} />
+        </div>
+        <div className="testimonies-content testimonieNormal">
           <Testimonies
             image="Bigboss"
             name="Big boss"
