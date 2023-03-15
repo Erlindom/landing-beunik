5c992b5b530f72b95c7081920c898438d5bf90b8
Erlin Dominguez <erlind299@gmail.com>
2023-03-14 19:56:52 -0500

    Adding new section in download app

--- src/components/DownloadApp/Download.jsx
+++ src/components/DownloadApp/Download.jsx
@@ -1,10 +1,39 @@
 import React from "react";
 import "../DownloadApp/Download.css";
 import DownloadBtn from "../DownladButton/DownloadBtn";
+import IconStep1 from "../Images/icon_step1.svg";
+import IconStep2 from "../Images/icon_step2.svg";
+import IconStep3 from "../Images/icon_step3.svg";
+import IconStep4 from "../Images/icon_step4.svg";
 
 function Download() {
   return (
     <section className="download_section" id="download">
+      <div className="steps_download_content">
+        <h4 className="title_stepsDownload">
+          Descarga la App y empieza tu mes gratis
+        </h4>
+        <div className="steps_download">
+          <div className="stepp">
+            <img className="icon_steps" src={IconStep1} alt="" />
+            <p className="text_step">Descarga</p>
+          </div>
+          <div className="stepp">
+            <img className="icon_steps" src={IconStep2} alt="" />
+            <p className="text_step">Regístrate</p>
+          </div>
+          <div className="stepp">
+            <img className="icon_steps" src={IconStep3} alt="" />
+            <p className="text_step">Configura</p>
+          </div>
+          <div className="stepp">
+            <img className="icon_steps" src={IconStep4} alt="" />
+            <p className="text_step">
+              Comparte <br /> tu perfil
+            </p>
+          </div>
+        </div>
+      </div>
       <div className="download_app">
         <div className="info_content">
           <h4 className="title_downloadApp">
